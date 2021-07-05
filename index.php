db9a62f63167727edd8546b2c88daeaf159e2cc9
fienifieucc <85882795+fienifieucc@users.noreply.github.com>
2021-07-05 18:30:22 +0000

    Update index.php

--- index.php
+++ index.php
@@ -368,7 +368,7 @@ h2{
            </button>
      </form>
        <form action="signup.html" method="post">
-<!--            <button type="submit" id="fa-cart-plus" name="viewcart"><br><i style="font-size: 18px; margin-left: 5px;" class="fa fa-sign-in" ></i><br> -->
+           <button type="submit" id="fa-cart-plus" name="viewcart"><i style="font-size: 18px; margin-left: 5px;" class="fa fa-sign-in" ></i><br>
             <div style="font-size: 14px" class="nav-name">About</div>
            </button>
        </form>
